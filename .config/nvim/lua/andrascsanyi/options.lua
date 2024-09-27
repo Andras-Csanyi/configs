2973b944fc946e08a501cbfc8e4c6ad29d346f6c
András Csányi <andras.csanyi@booking.com>
2024-09-27 13:36:11 +0200

    nvim: options

--- .config/nvim/lua/andrascsanyi/options.lua
+++ .config/nvim/lua/andrascsanyi/options.lua
@@ -1,7 +1,23 @@
 -- config
 local options = {
+  -- NeoVim will backup the file currently editing, but the backup
+  -- file won't be left in the filesystem once the writing of the 
+  -- file finished
 	backup = false,
+  writebackup = true,
+
+  -- how NeoVim manages making backup copies fromt the edited files
+  backupcopy = "auto",
+
+  -- when the bell is disabled
+  belloff = "all",
+  errorbell = off,
+
+  -- if NeoVim will use swap file or not
+  -- directory option defines where these files will be put
+  -- directory is at default state
 	swapfile = false,
+
 	undofile = true,
 	fileencoding = "utf-8",
 	clipboard = "unnamedplus",             -- allows access to system keyboard
@@ -10,19 +26,31 @@ local options = {
 	ignorecase = true,
 	pumheight = 10,
 	showtabline = true,
-	expandtab = true,
-	shiftwidth = 2,
-	tabstop = 2,
+
+  -- highlighting the line where the cursor in and how
 	cursorline = true,
+  cursorlineopt = "line",
+
+  ----------------------------
+	-- global editing options --
+  ----------------------------
+
+  -- use spaces when hit TAB
+	expandtab = true,
+  -- how many spaces are inserted when hit TAB
+	tabstop = 2,
+
+	shiftwidth = 2,
 	number = true,
 	relativenumber = true,
 	numberwidth = 4,
-	signcolumn = "yes",
+  signcolumn = "yes",
 	wrap = true,
 	linebreak = true,
 	scrolloff = 8,
 	sidescrolloff = 8,
 	showtabline = 2
+
 }
 
 for k, v in pairs(options) do
