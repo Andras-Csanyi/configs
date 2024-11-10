4f91d2d0a524bb8b45c417ddf3fd5341b4b63eb2
Andras Csanyi <andras.csanyi@pm.me>
2024-11-10 18:13:57 +0100

    nvim: Toggleterm fix

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -1,11 +1,12 @@
 return {
   "akinsho/toggleterm.nvim",
-  config = true,
-  cmd = "ToggleTerm",
+  lazy = true,
+  cmd = { "ToggleTerm" },
   keys = {
     { "<C-\\>", "<cmd>ToggleTerm<cr>", desc = "Toggle floating terminal" },
   },
   opts = {
+    open_mapping = [[<c-\>]],
     direction = "float",
     insert_mappings = true,
     terminal_mappings = true,
