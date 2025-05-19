c6e21ca709e28f46157bf3a0bd4c15df6681ed4a
Andras Csanyi <andras.csanyi@pm.me>
2025-05-19 12:39:19 +0200

    lazy dap ui

--- .config/nvim/lua/plugins/dap-ui.lua
+++ .config/nvim/lua/plugins/dap-ui.lua
@@ -7,13 +7,13 @@ return {
   },
   {
     "rcarriga/nvim-dap-ui",
-    dependencies = {
-      "nvim-neo-tree/neo-tree.nvim",
-    },
+    -- dependencies = {
+    --   "nvim-neo-tree/neo-tree.nvim",
+    -- },
     config = function()
       local dap = require("dap")
       local dapui = require("dapui")
-      local neotree_api = require("neo-tree.api")
+      -- local neotree_api = require("neo-tree.api")
 
       local dapui_layout_options = {
         layouts = {
@@ -44,7 +44,7 @@ return {
         dapui.open()
       end
       dap.listeners.before.launch.dapui_config = function()
-        neotree_api.tree.close()
+        -- neotree_api.tree.close()
         dapui.open()
       end
       dap.listeners.before.event_terminated.dapui_config = function()
@@ -52,7 +52,7 @@ return {
       end
       dap.listeners.before.event_exited.dapui_config = function()
         dapui.close()
-        neotree_api.tree.open()
+        -- neotree_api.tree.open()
       end
     end,
   },
