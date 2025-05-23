461e29b4f39fec339ace9ae2ae3756b0d458f997
Andras Csanyi <andras.csanyi@pm.me>
2025-05-23 13:42:27 +0200

    removing lvim, adding lazy keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set({ "n", "v" }, "<leader>j", "50j", { noremap = true, desc = "Jump 50 lines back." })
+vim.keymap.set({ "n", "v" }, "<leader>k", "50k", { noremap = true, desc = "Jump 50 lines ahead." })
